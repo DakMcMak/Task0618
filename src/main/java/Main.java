8de70d3265d5a7f1dea1723f708657b8fa67ff4a
DakMcMak <esbolov.galymzhan@mail.ru>
2017-05-05 21:07:50 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        KissMyShinyMetalAss kissMyShinyMetalAss = new KissMyShinyMetalAss();
+        System.out.println(kissMyShinyMetalAss);
+    }
+}
